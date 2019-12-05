5e90d1fe8e6f0aa596bd185520ff216f80838b8b
lukas111213 <58438364+lukas111213@users.noreply.github.com>
2019-12-05 20:24:41 +0100

    new dist

---
+++ dist2/precache-manifest.0f0e96d3c55ea6d214e4a006e5ec3ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525866d5b74a7e11cfb6",
+    "url": "/css/app.a897f003.css"
+  },
+  {
+    "revision": "f525227e7fd6a526de33bb32c566a7a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "525866d5b74a7e11cfb6",
+    "url": "/js/app.984e5181.js"
+  },
+  {
+    "revision": "8c928d2def68e9eb35f7",
+    "url": "/js/chunk-vendors.f26922f2.js"
+  },
+  {
+    "revision": "43838f8cfd90a90434d7c928dee98d65",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
